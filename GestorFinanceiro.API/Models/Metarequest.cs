36a6ecd638013330855dd4a41b5e21d20c742e89
Pedrocoelho13 <coelhopedro000@gmail.com>
2026-06-25 16:42:32 +0100

    criação de endpoints da api para as metas

---
+++ GestorFinanceiro.API/Models/Metarequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestorFinanceiro.API.Models
+{
+    public class MetaRequest
+    {
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        public string? Descricao { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor alvo deve ser maior que zero.")]
+        public decimal ValorAlvo { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal ValorAtual { get; set; } = 0;
+
+        [Required]
+        public int UtilizadorId { get; set; }
+    }
+}
