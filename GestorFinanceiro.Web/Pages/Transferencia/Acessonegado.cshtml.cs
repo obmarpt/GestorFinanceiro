a889a084a111e798245cb91953f33d73fdb645a1
Pedrocoelho13 <coelhopedro000@gmail.com>
2026-06-26 18:46:34 +0100

    Implementação da role de admin , com permissão para apagar users e poder tornar outros users admin

---
+++ GestorFinanceiro.Web/Pages/Transferencia/Acessonegado.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestorFinanceiro.Web.Pages.Transferencia
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
