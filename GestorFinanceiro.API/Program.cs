9ae12b0f188047ee82312c3b5e404afa907f3e3c
Obmar <duarterc2016@gmail.com>
2026-06-27 19:25:56 +0100

    fix endpoints

--- GestorFinanceiro.API/Program.cs
+++ GestorFinanceiro.API/Program.cs
@@ -32,6 +32,7 @@ app.UseRouting();
 app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapGet("/", () => "API running");
 app.MapControllers();
 app.MapHub<FinanceHub>("/financeHub");
 
